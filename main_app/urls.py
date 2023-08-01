419a1be0828db6754af239cc70bb37c5ac2b09f4
shaneyoung91 <shaneadam.young@gmail.com>
2023-08-01 10:30:33 -0700

    Add Django authorization

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
     path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
     path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Dog
 
 # Create your views here.
@@ -9,25 +13,50 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+
+@login_required
 def dogs_index(request):
-    dogs = Dog.objects.all()
+    dogs = Dog.objects.filter(user=request.user)
     return render(request, 'dogs/index.html', {
         'dogs' : dogs
     })
 
+
+@login_required
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   return render(request, 'dogs/detail.html', { 'dog': dog })
 
-class DogCreate(CreateView):
-  model = Dog
-  fields = '__all__'
-  success_url = '/dogs/'
 
-class DogUpdate(UpdateView):
+class DogCreate(LoginRequiredMixin, CreateView):
+  model = Dog
+  fields = ['name', 'breed', 'age', 'weight', 'diet', 'vaccinated']
+  
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+
+class DogUpdate(LoginRequiredMixin, UpdateView):
   model = Dog
   fields = ['breed', 'age', 'weight', 'diet', 'vaccinated']
 
-class DogDelete(DeleteView):
+
+class DogDelete(LoginRequiredMixin, DeleteView):
   model = Dog
   success_url = '/dogs'
+  
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
