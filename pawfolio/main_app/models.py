2be247daa7aa9b47a4b529c6727ab3c7b54f40db
lauren-m-johnson <laurenmj90@gmail.com>
2023-07-31 18:53:17 -0700

    add dog model

--- pawfolio/main_app/models.py
+++ pawfolio/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=50)
+    weight = models.IntegerField()
+    diet = models.TextField(max_length=250)
+    vaccinated = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
--- pawfolio/main_app/views.py
+++ pawfolio/main_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import Dog
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
+
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {
         'dogs' : dogs
     })
